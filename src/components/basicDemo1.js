53ad57e76e3b1121828f4ab1b6b78aadb27598f7
liuy <liuyang@bjyn.com>
2018-02-26 17:56:31 +0800

    基础图表一添加注释

--- src/components/basicDemo1.js
+++ src/components/basicDemo1.js
@@ -11,42 +11,67 @@ import * as d3 from 'd3';
 
 class BasicDemo1 extends Component {
     componentDidMount() {
+        /**
+         * d3.schemeCategory10:
+         * https://github.com/d3/d3-scale-chromatic/blob/master/README.md#categorical
+         */
         const colorList = d3.schemeCategory10;
+
         const width = 400,
             height = 400;
 
+        /**
+         * init svg, selection类型
+         */
         const svg = d3.select('#basicDemo1')
             .append('svg')
             .attr('width', 600)
             .attr('height', 600)
             .attr('class', 'ckt_svg_barChart');
 
-        // x轴比例尺
+        /**
+         * x轴刻度 - 种类
+         * 返回值是函数
+         */
         const xScale = d3.scaleBand()
             .rangeRound([0, width])
-            .padding(0.3)
+
+            // padding设置的值相当于占1step的比例
+            // 其中step为band最左边到相邻band最左边的距离
+            .paddingInner(0.3)
+            .paddingOuter(0.3)
             .domain(data.map(function (d) {
                 return d.name;
             }));
 
-        // y轴比例尺
+
+        /**
+         * y轴比例尺 - 数值
+         * 返回值是函数
+         */
         const yScale = d3.scaleLinear()
             .rangeRound([height, 0])
             .domain([0, d3.max(data.map(function (d) {
-                return d.value + 30
+                return d.value + 30;
             }))]);
 
         // group
         const group = svg.append('g')
             .attr('transform', 'translate(40, 40)');
 
-        // x轴
+        /**
+         * 添加x轴
+         * xAxis是selection类型
+         * selection.call方法:
+         * https://github.com/d3/d3-selection/blob/master/README.md#selection_call
+         */
         const xAxis = group.append('g')
             .attr('class', 'xAxis_barChart')
             .attr('transform', 'translate(0, ' + height + ')')
-            .call(d3.axisBottom(xScale));   // .call 方法
+            .call(d3.axisBottom(xScale));
 
-        // y轴
+
+        // 添加y轴
         const yAxis = group.append('g')
             .attr('class', 'yAxis_barChart')
             .call(d3.axisLeft(yScale));
@@ -54,27 +79,47 @@ class BasicDemo1 extends Component {
 
         // 渲染柱子
         group.selectAll('.bar')
+
+            // 关联数据
             .data(data)
             .enter()
+
+            // 添加柱子
             .append('rect')
             .attr('class', 'bar')
+
+            // 柱子的x位置
             .attr('x', function (d) {
                 return xScale(d.name);
             })
+
+            // 柱子的y位置，暂时设置为最底部
             .attr('y', height)
+
+            // 柱子的高度，暂时设置为1
+            .attr('height', 1)
+
+            // 柱子的宽度
             .attr('width', xScale.bandwidth())
-            .attr('height', 1)   // 为什么
+
+            // 填充颜色
             .attr('fill', function (d, i) {
                 return colorList[i];
             })
+
+            // 设置动画
             .transition()
             .duration(1000)
             .delay(function (d, i) {
                 return i * 500 / 3;
             })
+
+            // 重新设置一次高度，产生动画效果
             .attr('height', function (d) {
                 return height - yScale(d.value);
             })
+
+            // 重新设置一次y位置，产生动画效果
             .attr('y', function (d) {
                 return yScale(d.value);
             });
